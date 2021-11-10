96d62cd0b8840f5903142059e31fe4cfcecfb0ce
Xabier Gabiña <33607963+Xabierland@users.noreply.github.com>
2021-11-10 18:53:33 +0100

    .

---
+++ src/tema_2/cartas/Baraja.java
@@ -0,0 +1,18 @@
+package tema_2.cartas;
+
+import java.util.Iterator;
+
+public class Baraja
+{
+    private Carta[] cartas;
+
+    public Baraja()
+    {
+
+    }
+
+    public Iterator<Carta> iterador()
+    {
+
+    }
+}
---
+++ src/tema_2/cartas/Carta.java
@@ -0,0 +1,7 @@
+package tema_2.cartas;
+
+public class Carta
+{
+    String palo;
+    int valor;
+}
