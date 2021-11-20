382c194c524739a5926162476833fde10f06c411
Xabier Gabiña <33607963+Xabierland@users.noreply.github.com>
2021-11-20 12:36:33 +0100

    .

--- src/tema_2/juego_bloques/Bloque.java
+++ src/tema_2/juego_bloques/Bloque.java
@@ -3,4 +3,10 @@ package tema_2.juego_bloques;
 public class Bloque {
     int puntos;
     int salto; //valor entre -3 y +3
+
+    public Bloque(int p, int s)
+    {
+        puntos=p;
+        salto=s;
+    }
 }
