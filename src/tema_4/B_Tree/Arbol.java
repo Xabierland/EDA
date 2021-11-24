5c4663d1bd0af967afedee0aefd8683fced80ae9
Xabier Gabiña <33607963+Xabierland@users.noreply.github.com>
2021-11-24 16:32:58 +0100

    .

---
+++ src/tema_4/B_Tree/Arbol.java
@@ -0,0 +1,13 @@
+package tema_4.B_Tree;
+
+public class Arbol
+{
+    BTN<Integer> root;
+
+    public boolean esta(Integer elem)
+    {
+        //post: el resultado es true si elem esta en el arbol
+        //      y false si no.
+
+    }
+}
---
+++ src/tema_4/B_Tree/BTN.java
@@ -0,0 +1,7 @@
+package tema_4.B_Tree;
+
+public class BTN<T>
+{
+    T[] valores;
+    BTN<T>[] hijos; //el tamaño es de la tabla de valores +1
+}
