ffc4144b0e233bcaed000a9166681a29b2e7fb2d
Xabier Gabiña <33607963+Xabierland@users.noreply.github.com>
2021-12-18 11:21:15 +0100

    .

--- src/tema_5/simular_epidemia/Epidemia.java
+++ src/tema_5/simular_epidemia/Epidemia.java
@@ -1,26 +1,68 @@
 package tema_5.simular_epidemia;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Random;
+import java.util.*;
 
 public class Epidemia
 {
-    protected final int DEFAULT_CAPACITY = 10;
-    protected LinkedList<Persona>[] adjList;
-
+    Persona Jon; Persona Ana; Persona Isa; Persona Peio; Persona Iker; Persona Peru; Persona Fer; Persona Tim; Persona Elsa; Persona Dan;
     public Epidemia()
     {
-        adjList=new LinkedList[DEFAULT_CAPACITY];
-        for(int i=0;i<DEFAULT_CAPACITY;i++)
-        {
-            adjList[i]=new LinkedList<>();
-        }
+        Jon=new Persona("Jon");
+        Ana=new Persona("Ana");
+        Isa=new Persona("Isa");
+        Peio=new Persona("Peio");
+        Iker=new Persona("Iker");
+        Peru=new Persona("Peru");
+        Fer=new Persona("Fer");
+        Tim=new Persona("Tim");
+        Elsa=new Persona("Elsa");
+        Dan=new Persona("Dan");
+        Jon.contactos.add(Ana);Jon.contactos.add(Peru);
+        Ana.contactos.add(Peio);Ana.contactos.add(Isa);Ana.contactos.add(Jon);
+        Isa.contactos.add(Ana);Isa.contactos.add(Iker);Isa.contactos.add(Dan);
+        Peio.contactos.add(Ana);Peio.contactos.add(Iker);
+        Iker.contactos.add(Peio);Iker.contactos.add(Isa);Iker.contactos.add(Fer);Iker.contactos.add(Peru);
+        Peru.contactos.add(Jon);Peru.contactos.add(Fer);Peru.contactos.add(Tim);Peru.contactos.add(Iker);
+        Tim.contactos.add(Peru);
+        Fer.contactos.add(Iker);Fer.contactos.add(Peru);Fer.contactos.add(Elsa);
+        Elsa.contactos.add(Fer);Elsa.contactos.add(Dan);
+        Dan.contactos.add(Elsa);Dan.contactos.add(Isa);
+
     }
 
     public void simularEpidemia()
     {
+        Queue<Persona> porEx=new LinkedList<>();
+        HashSet<Persona> examinados=new HashSet<>();
 
+        Jon.infectada=true;
+        System.out.println(Jon.nombre+" \tHA SIDO INFECTO\n");
+        porEx.add(Jon);
+        examinados.add(Jon);
+
+        while (!porEx.isEmpty())
+        {
+            Persona act=porEx.remove();
+            for(Persona p:act.contactos)
+            {
+                if(!examinados.contains(p))
+                {
+                    if(act.infectada&&contagio())
+                    {
+                        p.infectada=true;
+                        porEx.add(p);
+                        examinados.add(p);
+                        System.out.println(act.nombre+" \tSI HA INFECTADO A  \t"+p.nombre);
+                    }
+                    else
+                    {
+                        porEx.add(p);
+                        examinados.add(p);
+                        System.out.println(act.nombre+ " \tNO HA INFECTADO A \t"+p.nombre);
+                    }
+                }
+            }
+        }
     }
 
     public boolean contagio()
@@ -39,5 +81,12 @@ public class Epidemia
         String nombre;
         boolean infectada; //Al principio false
         ArrayList<Persona> contactos;
+
+        public Persona(String nom)
+        {
+            nombre=nom;
+            infectada=false;
+            contactos=new ArrayList<>();
+        }
     }
 }
